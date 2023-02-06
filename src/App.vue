c0253156c6c51d3a9319acd55e047f7df5b4e5ed
Jordan Andrzejczak <jandrzejczak@afsgroup.nl>
2023-02-06 15:36:26 +0100

    3d object test

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,89 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+
+let camera, scene, renderer;
+
+init();
+render();
+
+function init() {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.25,
+    20
+  );
+  camera.position.set(-1.8, 0.6, 2.7);
+
+  scene = new THREE.Scene();
+
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("../public/jsm/libs/draco/");
+
+  new RGBELoader()
+    .setPath("../public/")
+    .load("royal_esplanade_1k.hdr", function (texture) {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+
+      scene.environment = texture;
+
+      render();
+
+      // model
+      const loader = new GLTFLoader().setPath("../public/models/");
+      loader.setDRACOLoader(dracoLoader);
+      loader.load("face.glb", function (gltf) {
+        scene.add(gltf.scene);
+
+        render();
+      });
+    });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener("change", render); // use if there is no animation loop
+  controls.minDistance = 0;
+  controls.maxDistance = 10;
+  controls.target.set(0, 0, -0.2);
+  controls.update();
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+//
+
+function render() {
+  renderer.render(scene, camera);
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="wrapper"></div>
 </template>
 
 <style scoped>
