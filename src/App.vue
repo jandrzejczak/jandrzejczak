b0e82ea16bc718b4bca14edc8d6aa963c89ea8d5
jandrzejczak <jordan.andrzejczak@custommerce.pl>
2023-02-06 20:08:45 +0100

    Object as background

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
-import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
-
-let camera, scene, renderer;
-
-init();
-render();
-
-function init() {
-  const container = document.createElement("div");
-  document.body.appendChild(container);
-
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.25,
-    20
-  );
-  camera.position.set(-1.8, 0.6, 2.7);
-
-  scene = new THREE.Scene();
-
-  const dracoLoader = new DRACOLoader();
-  dracoLoader.setDecoderPath("../public/jsm/libs/draco/");
-
-  new RGBELoader()
-    .setPath("../public/")
-    .load("royal_esplanade_1k.hdr", function (texture) {
-      texture.mapping = THREE.EquirectangularReflectionMapping;
-
-      scene.environment = texture;
-
-      render();
-
-      // model
-      const loader = new GLTFLoader().setPath("../public/models/");
-      loader.setDRACOLoader(dracoLoader);
-      loader.load("face.glb", function (gltf) {
-        scene.add(gltf.scene);
-
-        render();
-      });
-    });
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 1;
-  renderer.outputEncoding = THREE.sRGBEncoding;
-  container.appendChild(renderer.domElement);
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.addEventListener("change", render); // use if there is no animation loop
-  controls.minDistance = 0;
-  controls.maxDistance = 10;
-  controls.target.set(0, 0, -0.2);
-  controls.update();
-
-  window.addEventListener("resize", onWindowResize);
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  render();
-}
-
-//
-
-function render() {
-  renderer.render(scene, camera);
-}
+import FaceBackground from "./components/FaceBackground.vue";
+import Loading from "./components/Loading.vue";
 </script>
 
 <template>
-  <div class="wrapper"></div>
+<loading></loading>
+  <face-background></face-background>
 </template>
 
 <style scoped>
@@ -148,4 +72,8 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
+</style>
+
+<style lang="scss">
+  @import './assets/styles/main.scss';
 </style>
---
+++ src/components/FaceBackground.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { onMounted } from "vue";
+
+let camera, scene, renderer;
+
+onMounted(() => {
+  init();
+  render();
+});
+
+function init() {
+  var container = document.getElementById("face");
+
+  console.log(container);
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.25,
+    20
+  );
+  camera.position.set(-1.8, 0.6, 2.7);
+
+  scene = new THREE.Scene();
+
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("../public/jsm/libs/draco/");
+
+  new RGBELoader()
+    .setPath("../public/")
+    .load("royal_esplanade_1k.hdr", function (texture) {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+
+      scene.environment = texture;
+
+      render();
+
+      // model
+      const loader = new GLTFLoader().setPath("../public/models/");
+      loader.setDRACOLoader(dracoLoader);
+      loader.load("face.glb", function (gltf) {
+        console.log("object ready");
+        scene.add(gltf.scene);
+
+        render();
+      });
+    });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setClearColor(0x000000, 0); // the default
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener("change", render); // use if there is no animation loop
+  controls.minDistance = 0;
+  controls.maxDistance = 10;
+  controls.target.set(0, 0, -0.2);
+  controls.update();
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+</script>
+
+<template>
+  <div id="face" class="face-bg"></div>
+</template>
+
+<style scoped>
+.face-bg {
+  z-index: -1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
